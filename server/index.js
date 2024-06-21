0c2f17e69f31803d76f3231ba72ec489914eace7
Giridev24 <140387591+Giridev24@users.noreply.github.com>
2024-06-21 18:31:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors(
   {
-  origin: frontendURL, // Allow requests from this origin
+  origin: [''], // Allow requests from this origin
   methods: ['GET', 'POST', 'DELETE'], // Allow these HTTP methods
   allowedHeaders: ['Content-Type'], // Allow these headers
 }
