2de3b6bb547f058ee865c81059c9554017db428d
Giridev24 <140387591+Giridev24@users.noreply.github.com>
2024-06-21 21:56:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors(
   {
   origin: ['https://master--24todo-client.netlify.app/'], // Allow requests from this origin
-  methods: ['GET', 'POST', 'DELETE'],
+  methods: ['GET', 'POST', 'DELETE', 'PUT'],
   credentials: true
 }
 ));
