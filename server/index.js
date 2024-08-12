c4984b48e7570aa9d5789bd7cd5062bd99bd965e
Giridev24 <140387591+Giridev24@users.noreply.github.com>
2024-08-12 10:21:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const uri = process.env.MONGO_URL;
 
 // CORS options
 const corsOptions = {
-  origin: 'https://todo-client-r6m4.onrender.com', // Allow requests only from this origin
+  origin: 'https://todo-pied-three.vercel.app', // Allow requests only from this origin
   methods: ['GET', 'POST', 'DELETE', 'PUT'],
   credentials: true// Allow these methods
 };
